a44e17dbae914d5b8a8fd9adaac82653e765c40f
NaulaN <nonochrzaszcz@gmail.com>
2023-05-21 14:37:56 +0200

    Player JUnit test side

--- src/test/java/fr/iut/montreuil/S4_R04_2023_40_QuizMoyai/UserTest.java
+++ src/test/java/fr/iut/montreuil/S4_R04_2023_40_QuizMoyai/UserTest.java
@@ -1,10 +1,101 @@
 package fr.iut.montreuil.S4_R04_2023_40_QuizMoyai;
 
-import junit.framework.TestCase;
+import fr.iut.montreuil.S4_R04_2023_40_QuizMoyai.entities.dto.UserDTO;
+import fr.iut.montreuil.S4_R04_2023_40_QuizMoyai.exceptions.*;
+import fr.iut.montreuil.S4_R04_2023_40_QuizMoyai.modeles.UserInterface;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-public class UserTest extends TestCase
-{
-    public final void testCreateUser() {
-        assertTrue(true);
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserTest {
+    @Mock
+    public static UserInterface userImpl;
+
+    @BeforeAll
+    public static void initAll() {
+        userImpl = new UserMoc();
+    }
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testAddUser_ValidInput_Success() {
+        Mockito.when(userImpl.createUser("john123",
+                                         "John Doe",
+                                         0,
+                                         1990,
+                                         "Reading,Sports")).thenReturn(new UserDTO("john123",
+                                                                                   "John Doe",
+                                                                                   1990,
+                                                                                   "Reading,Sports",
+                                                                                   0));
+        assertDoesNotThrow(() -> userImpl.createUser("john123", "John Doe", 0, 1990, "Reading,Sports"));
+    }
+
+    @Test
+    public void testAddUser_BlankFirstName_ExceptionThrown() {
+        Mockito.when(userImpl.createUser("john123",
+                                         "",
+                                         0,
+                                         1990,
+                                         "Reading,Sports")).thenThrow(BlankFirstNameException.class);
+        assertThrows(BlankFirstNameException.class, () ->
+                userImpl.createUser("john123", "", 0, 1990, "Reading,Sports"));
+    }
+
+    @Test
+    public void testAddUser_InvalidPreferenceLanguage_ExceptionThrown() {
+        Mockito.when(userImpl.createUser("john123",
+                                         "John Doe",
+                                         -1,
+                                         1990,
+                                         "Reading,Sports")).thenThrow(InvalidPreferenceLanguageException.class);
+        assertThrows(InvalidPreferenceLanguageException.class, () ->
+                userImpl.createUser("john123", "John Doe", -1, 1990, "Reading,Sports"));
+    }
+
+    @Test
+    public void testAddUser_InvalidBirthYear_ExceptionThrown() {
+        Mockito.when(userImpl.createUser("john123",
+                                         "John Doe",
+                                         0,
+                                         1800,
+                                         "Reading,Sports")).thenThrow(InvalidBirthYearException.class);
+        assertThrows(InvalidBirthYearException.class, () ->
+                userImpl.createUser("john123", "John Doe", 0, 1800, "Reading,Sports"));
+    }
+
+    @Test
+    public void testAddUser_AlreadyUsedPseudo_ExceptionThrown() {
+        userImpl.createUser("john123", "John Doe", 0, 1990, "Reading,Sports");
+        Mockito.when(userImpl.createUser("john123",
+                                         "Jane Smith",
+                                         1,
+                                         1995,
+                                         "Music")).thenThrow(AlreadyUsedPseudoException.class);
+        assertThrows(AlreadyUsedPseudoException.class, () ->
+                userImpl.createUser("john123", "Jane Smith", 1, 1995, "Music"));
+    }
+
+    @Test
+    public void testAddUser_InvalidPseudoFormat_ExceptionThrown() {
+        Mockito.when(userImpl.createUser("john 123",
+                                         "John Doe",
+                                         0,
+                                         1990,
+                                         "Reading,Sports")).thenThrow(AlreadyUsedPseudoException.class);
+        assertThrows(AlreadyUsedPseudoException.class, () ->
+                userImpl.createUser("john 123", "John Doe", 0, 1990, "Reading,Sports"));
     }
 }
