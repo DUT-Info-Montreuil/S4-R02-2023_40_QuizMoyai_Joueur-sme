d32c11661b4f3ac23386c34b72fe29509bd35364
NaulaN <nonochrzaszcz@gmail.com>
2023-05-22 14:04:46 +0200

    Added Exceptions, StatDTO and update UserInterface, UserDTO

---
+++ src/main/java/fr/iut/montreuil/S4_R04_2023_40_QuizMoyai/exceptions/UserNotFoundException.java
@@ -0,0 +1,4 @@
+package fr.iut.montreuil.S4_R04_2023_40_QuizMoyai.exceptions;
+
+public class UserNotFoundException extends Exception {
+}
--- src/main/java/fr/iut/montreuil/S4_R04_2023_40_QuizMoyai/modeles/UserInterface.java
+++ src/main/java/fr/iut/montreuil/S4_R04_2023_40_QuizMoyai/modeles/UserInterface.java
@@ -1,8 +1,12 @@
 package fr.iut.montreuil.S4_R04_2023_40_QuizMoyai.modeles;
 
+import fr.iut.montreuil.S4_R04_2023_40_QuizMoyai.entities.dto.StatDTO;
 import fr.iut.montreuil.S4_R04_2023_40_QuizMoyai.entities.dto.UserDTO;
 import fr.iut.montreuil.S4_R04_2023_40_QuizMoyai.exceptions.*;
 
+import java.util.List;
+
 public interface UserInterface {
     public UserDTO createUser(String firstName, String pseudo, int preferenceLanguage, int birthYear, String hobbies) throws AlreadyUsedPseudoException, BlankFirstNameException, FirstNameFormatException, InvalidBirthYearException, InvalidPreferenceLanguageException, PseudoFormatException, BlankPseudoException;
+    public List<StatDTO> fournirStatsUser(String pseudo) throws UserNotFoundException;
 }
